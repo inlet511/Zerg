a6f4227d1bf283a1cdff64b67fc6571aa13efeb8
Ken <80669349@qq.com>
2018-03-17 23:51:07 +0800

    0317

--- application/route.php
+++ application/route.php
@@ -20,6 +20,8 @@ Route::get('api/:version/product/recent','api/:version.Product/getRecent');
 
 Route::get('api/:version/category/all','api/:version.Category/getAllCategories');
 Route::post('api/:version/token/user','api/:version.Token/getToken');
+Route::post('api/:version/token/verify','api/:version.Token/verifyToken');
+
 
 Route::post('api/:version/address','api/:version.Address/createOrUpdateAddress');
 
